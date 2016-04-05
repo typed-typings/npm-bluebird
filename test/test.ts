27fae29962060fddc8d504ee50bf3970724a64e7
Louy <louy08@gmail.com>
2016-04-05 22:27:13 +0100

    Update typings

--- test/test.ts
+++ test/test.ts
@@ -1,12 +1,12 @@
 /// <reference path="./main.d.ts" />
 /// <reference path="../typings/main.d.ts" />
 import test = require('blue-tape');
-import BlueBirdPromise = require('bluebird');
+import Bluebird = require('bluebird');
 
 test('Promise.ctor', (t) => {
-  return new BlueBirdPromise((resolve, reject) => {
+  return new Bluebird<void>((resolve, reject) => {
     t.assert(typeof resolve === 'function', 'resolve is a function');
     t.assert(typeof reject === 'function', 'reject is a function');
-    resolve();
+    resolve(null);
   });
 });
